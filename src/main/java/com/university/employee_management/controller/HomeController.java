9cc090d05fc3dc0fcfb7ad85e6b2846a26555744
Shreya Ydv <ydvkhushi21@gmail.com>
2026-01-15 02:00:56 +0530

    Fix: remove profile restriction causing startup crash

--- src/main/java/com/university/employee_management/controller/HomeController.java
+++ src/main/java/com/university/employee_management/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@Profile("render")
+
 public class HomeController {
 
     @GetMapping("/")
