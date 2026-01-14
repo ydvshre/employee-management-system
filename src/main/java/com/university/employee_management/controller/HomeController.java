963da206719bfe54eb5dad971515095ec5493ebc
Shreya Ydv <ydvkhushi21@gmail.com>
2026-01-15 00:59:39 +0530

    Add login route mapping

--- src/main/java/com/university/employee_management/controller/HomeController.java
+++ src/main/java/com/university/employee_management/controller/HomeController.java
@@ -3,6 +3,7 @@ import org.springframework.context.annotation.Profile;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Profile("render")
@@ -12,5 +13,15 @@ public class HomeController {
     public String index() {
         return "index";
     }
+    @GetMapping("/login")
+public String login(@RequestParam String role) {
+    if ("admin".equals(role)) {
+        return "redirect:/admin/home";
+    } else if ("subadmin".equals(role)) {
+        return "redirect:/subadmin/home";
+    }
+    return "index";
+}
+
 }
 
